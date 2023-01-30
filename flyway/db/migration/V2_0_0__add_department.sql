3e67b87c60588c70d6368f5fcecfccbc4556611c
Alexey Mazhukin <amazhukin@lineate.com>
2023-01-30 12:56:57 +0400

    Added simple flyway migration

---
+++ flyway/db/migration/V2_0_0__add_department.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `departments` (
+    `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` varchar(20),
+    `description` varchar(50)
+);
---
+++ flyway/db/migration/V2_1_0__link_users_and_departments.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `users` ADD `departments_id` int;
