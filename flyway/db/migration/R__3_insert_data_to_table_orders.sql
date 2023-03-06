46b503a49fc24bf144b64250812788cbb172ac94
Alexey Mazhukin <amazhukin@lineate.com>
2023-03-06 18:49:29 +0400

    TASK-1: fixed setting of old value

--- flyway/db/migration/R__3_insert_data_to_table_orders.sql
+++ flyway/db/migration/R__3_insert_data_to_table_orders.sql
@@ -1,2 +1,2 @@
-MERGE INTO orders (id, customer_id, total_price)
-VALUES (1, 1, 99.99), (2, 1, 99.99), (3, 1, 999.90);
+MERGE INTO orders (id, customer_id, total_price, status)
+VALUES (1, 1, 99.99, 'OLD'), (2, 1, 99.99, 'OLD'), (3, 1, 999.90, 'OLD');
--- flyway/db/migration/V4__add_column_status_with_value_to_table_orders.sql
+++ flyway/db/migration/V4__add_column_status_with_value_to_table_orders.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `orders` ADD `status` varchar(100);
 UPDATE orders SET status = 'OLD';
+ALTER TABLE `orders` ALTER COLUMN `status` varchar(100) NOT NULL;
